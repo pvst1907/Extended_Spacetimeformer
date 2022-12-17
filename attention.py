Norm & Softmax Fix

attention.py
@@ -2,21 +2,6 @@ import torch
 import torch.nn as nn
 
 
-class SafeSoftmax(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, X):
-        X_denom = torch.sum(torch.exp(X), 2) + 1e-8
-        if torch.isnan(X_denom).any():
-            print('B')
-            assert False
-        if torch.isnan(torch.divide(torch.exp(X), torch.unsqueeze(X_denom, dim=2))).any():
-            print(torch.min(X_denom))
-            assert False
-        return torch.divide(torch.exp(X + 1e-8), torch.unsqueeze(X_denom, dim=2))
-
-
 class LocalSelfAttention(nn.Module):
     def __init__(self, input_size, seq_length, embedding_size, qkv_size, masked=False):
 
@@ -95,7 +80,7 @@ class AttentionHead(nn.Module):
         # Dimensions W_V: (N_w*M) x (N_w*s_qkv)
         self.WV = nn.Linear(seq_length * self.emb_size, seq_length * self.qkv_size)
 
-        self.softmax = SafeSoftmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, sent_embed_slice):  # Dimension sent_embed_slice: (N_w x M)
         # Dimensions Q: (N_w x s_qkv)
@@ -168,7 +153,7 @@ class CrossAttentionHead(nn.Module):
         # Dimensions W_V: (N_w*M) x (N_w*s_qkv)
         self.WV = nn.Linear(src_seq_length * self.emb_size, src_seq_length * self.qkv_size)
 
-        self.softmax = SafeSoftmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, basic_decoder_slice, final_encoder_slice):  #Dimension basic_decoder_slice (source): (N_w x M), Dimension final_encoder_slice (target): (N_w x M)
         # Dimensions Q: (N_w x s_qkv)

decoder.py
@@ -19,27 +19,27 @@ class Decoder(nn.Module):
 
         self.target_embedding = EmbeddingGenerator(self.embedding_size_time, self.embedding_size_variable, self.embedding_size_sector, self.output_size, self.trg_seq_length)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm1 = nn.LayerNorm([self.trg_seq_length, self.embedding_size])
+        self.norm1 = nn.BatchNorm1d(self.embedding_size)
         # Self-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.local_attention_layer = LocalSelfAttention(self.output_size, self.trg_seq_length, self.embedding_size, self.s_qkv, masked=True)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm2 = nn.LayerNorm([self.trg_seq_length, self.embedding_size])
+        self.norm2 = nn.BatchNorm1d(self.embedding_size)
         # Self-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.global_attention_layer = GlobalSelfAttention(self.output_size, self.trg_seq_length, self.embedding_size, self.s_qkv, masked=True)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm3 = nn.LayerNorm([self.trg_seq_length, self.embedding_size])
+        self.norm3 = nn.BatchNorm1d(self.embedding_size)
         # Cross-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.cross_attention_layer_1 = CrossAttention(self.output_size, self.src_seq_length, self.trg_seq_length, self.embedding_size, self.s_qkv, masked=False)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm4 = nn.LayerNorm([self.trg_seq_length, self.embedding_size])
+        self.norm4 = nn.BatchNorm1d(self.embedding_size)
         # Cross-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.cross_attention_layer_2 = CrossAttention(self.output_size, self.src_seq_length, self.trg_seq_length, self.embedding_size, self.s_qkv, masked=False)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm5 = nn.LayerNorm([self.trg_seq_length, self.embedding_size])
+        self.norm5 = nn.BatchNorm1d(self.embedding_size)
         # FFN  (in: (N_w x M) out: (N_w x M))
         self.W1 = nn.Linear(self.s_qkv, self.s_qkv)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm6 = nn.LayerNorm([trg_seq_length, self.embedding_size])
+        self.norm6 = nn.BatchNorm1d(self.embedding_size)
         # Output Layer in: (N_w x M) out: (N_w x 1))
         self.output_layer = nn.Linear(in_features=xformer.s_qkv, out_features=xformer.output_size)
 
@@ -58,37 +58,37 @@ class Decoder(nn.Module):
         embedded_sequence_trg = self.target_embedding(sequence_trg_flat, time_index_sequence, variable_index_sequence,sector_index_sequence)
 
         # Norm
-        normed_sequence_trg = self.norm1(embedded_sequence_trg)
+        normed_sequence_trg = self.norm1(embedded_sequence_trg.transpose(2,1))
 
         # Local Self Attetion
-        local_attention_trg = self.local_attention_layer(normed_sequence_trg)
+        local_attention_trg = self.local_attention_layer(normed_sequence_trg.transpose(2,1))
 
         # Norm
-        normed_local_attention_trg = self.norm2(embedded_sequence_trg + local_attention_trg)
+        normed_local_attention_trg = self.norm2(local_attention_trg.transpose(2,1)+embedded_sequence_trg.transpose(2,1))
 
         # Global Self Attention
-        global_attention_trg = self.global_attention_layer(normed_local_attention_trg)
+        global_attention_trg = self.global_attention_layer(normed_local_attention_trg.transpose(2,1))
 
         # Norm
-        normed_global_attention_trg = self.norm3(local_attention_trg + global_attention_trg)
+        normed_global_attention_trg = self.norm3(global_attention_trg.transpose(2,1)+local_attention_trg.transpose(2,1))
 
         # Cross Attention 1st Layer
-        cross_attention_1 = self.cross_attention_layer_1(normed_global_attention_trg, sequence_src_flat)
+        cross_attention_1 = self.cross_attention_layer_1(normed_global_attention_trg.transpose(2,1), sequence_src_flat)
 
         # Norm
-        normed_cross_attention = self.norm4(global_attention_trg + cross_attention_1)
+        normed_cross_attention = self.norm4(cross_attention_1.transpose(2,1)+global_attention_trg.transpose(2,1))
 
         # Cross Attention 2nd Layer
-        cross_attention_2 = self.cross_attention_layer_2(normed_cross_attention, sequence_src_flat)
+        cross_attention_2 = self.cross_attention_layer_2(normed_cross_attention.transpose(2,1), sequence_src_flat)
 
         # Norm
-        normed_cross_attention_2 = self.norm5(cross_attention_1 + cross_attention_2)
+        normed_cross_attention_2 = self.norm5(cross_attention_2.transpose(2,1)+cross_attention_1.transpose(2,1))
 
         # Linear Layer & ReLU
-        decoder_out = nn.ReLU()(self.W1(normed_cross_attention_2))
+        decoder_out = nn.ReLU()(self.W1(normed_cross_attention_2.transpose(2,1)))
 
         # Norm
-        normed_decoder_out = self.norm6(cross_attention_2 + decoder_out)
+        normed_decoder_out = self.norm6(decoder_out.transpose(2,1)+cross_attention_2.transpose(2,1))
 
         # Linear Layer
-        return self.output_layer(normed_decoder_out)
+        return self.output_layer(normed_decoder_out.transpose(2,1))

encoder.py
@@ -19,19 +19,19 @@ class Encoder(nn.Module):
 
         self.context_embedding = EmbeddingGenerator(self.embedding_size_time, self.embedding_size_variable,self.embedding_size_sector, self.input_size, self.src_seq_length)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm1 = nn.LayerNorm([self.src_seq_length, self.embedding_size])
+        self.norm1 = nn.BatchNorm1d(self.embedding_size)
         # Self-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.local_attention_layer = LocalSelfAttention(self.input_size, self.src_seq_length, self.embedding_size, self.s_qkv)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm2 = nn.LayerNorm([self.src_seq_length, self.embedding_size])
+        self.norm2 = nn.BatchNorm1d(self.embedding_size)
         # Self-Attention Layer Local (in: (N_w x M) out: (N_w x M))
         self.global_attention_layer = GlobalSelfAttention(self.input_size, self.src_seq_length, self.embedding_size, self.s_qkv)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm3 = nn.LayerNorm([self.src_seq_length, self.embedding_size])
+        self.norm3 = nn.BatchNorm1d(self.embedding_size)
         # FFN  (in: (N_w x M) out: (N_w x M))
         self.W1 = nn.Linear(self.s_qkv, self.s_qkv)
         # Layer Norm  (in: (N_w x M) out: (N_w x M))
-        self.norm4 = nn.LayerNorm((self.src_seq_length, self.embedding_size))
+        self.norm4 = nn.BatchNorm1d(self.embedding_size)
 
     def forward(self, sequence):
 
@@ -46,24 +46,24 @@ class Encoder(nn.Module):
         embedded_sequence = self.context_embedding(sequence_flat, time_index_sequence, variable_index_sequence, sector_index_sequence)
 
         # Norm
-        normed_sequence = self.norm1(embedded_sequence)
+        normed_sequence = self.norm1(embedded_sequence.transpose(2,1))
 
         # Local Self Attention
-        local_attention = self.local_attention_layer(normed_sequence)
+        local_attention = self.local_attention_layer(normed_sequence.transpose(2,1))
 
         # Norm
-        normed_local_attention = self.norm2(embedded_sequence + local_attention)
+        normed_local_attention = self.norm2(local_attention.transpose(2,1)+embedded_sequence.transpose(2,1))
 
         # Global Self Attention NaN
-        global_attention = self.global_attention_layer(normed_local_attention)
+        global_attention = self.global_attention_layer(normed_local_attention.transpose(2,1))
 
         # Norm
-        normed_global_attention = self.norm3(local_attention + global_attention)
+        normed_global_attention = self.norm3(global_attention.transpose(2,1)+local_attention.transpose(2,1))
 
         # Linear Layer & ReLU
-        encoder_out = nn.ReLU()(self.W1(normed_global_attention))
+        encoder_out = nn.ReLU()(self.W1(normed_global_attention.transpose(2,1)))
 
         # Norm
-        normed_encoder_out = self.norm4(global_attention + encoder_out)
+        normed_encoder_out = self.norm4(encoder_out.transpose(2,1)+global_attention.transpose(2,1))
 
-        return normed_encoder_out
+        return normed_encoder_out.transpose(2,1)

train.py
@@ -14,7 +14,7 @@ def train_epoch(xformer,
     tracker = Accumulator(2)
     for src, trg, trg_y in train_iter:
         trg_y_hat = xformer(src, trg)
-        l = loss(trg_y, trg_y_hat)
+        l = loss(trg_y.transpose(2,1), trg_y_hat)
         encoder_optimizer.zero_grad()
         decoder_optimizer.zero_grad()
         l.mean().backward()
@@ -40,7 +40,7 @@ def train_torch(xformer,
                 plot=False):
     min_loss = np.nan
     epochs_no_improve = 0
-    torch.autograd.set_detect_anomaly(True)
+    #torch.autograd.set_detect_anomaly(True)
     if plot:
         animator = Animator(xlabel='epoch', xlim=[1, epochs], legend=['training loss', 'evaluation_loss'])
     for epoch in range(epochs):